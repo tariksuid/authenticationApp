bd7e5b335420e5d1b35a3372cf629654b5944027
tariksuid <tariksuid@gmail.com>
2019-06-26 01:16:48 +0300

    second Version

--- app/src/main/java/com/example/authenticationapp/HomeActivity.java
+++ app/src/main/java/com/example/authenticationapp/HomeActivity.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,15 +30,34 @@ import java.util.Map;
 
 public class HomeActivity<list> extends AppCompatActivity {
     FirebaseFirestore db;
-
-    List<CheckBox> list ;
-
+String tasks = "" ;
+    FirebaseUser user_id;
+    Button add;
+    EditText editText;
+    TextView todo;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         db = FirebaseFirestore.getInstance();
-     }
+        add = findViewById(R.id.addBtn);
+        editText = findViewById(R.id.taskTxt);
+todo = findViewById(R.id.tasksA);
+        readFromDB();
+
+        //read the DB
+        add.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                writeOnDB();
+            }
+        });
+
+        user_id = FirebaseAuth.getInstance().getCurrentUser();
+
+    }
 
     public void logOUT(View view) {
         startActivity(new Intent(this, MainActivity.class)); //Go back to home page
@@ -39,18 +65,16 @@ public class HomeActivity<list> extends AppCompatActivity {
     }
 
 
-    public void writeOnDB(View view) {
+    public void writeOnDB() {
         // Create a new user with a first and last name
         Map<String, Object> user = new HashMap<>();
 
-        FirebaseUser user_id = FirebaseAuth.getInstance().getCurrentUser();
-
-        user.put("username", "Tarik");
-        user.put("uId", user_id.getUid());
-        user.put("complete", false);
+         //  user.put("username", "Tarik");
+        user.put("tID", user_id.getUid());
+        user.put("task", editText.getText().toString());
 
         // Add a new document with a generated ID
-        db.collection("todos")
+        db.collection("tasks")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
@@ -67,4 +91,39 @@ public class HomeActivity<list> extends AppCompatActivity {
 
 
     }
+
+
+    void readFromDB() {
+        final FirebaseUser user_id = FirebaseAuth.getInstance().getCurrentUser();
+        db.collection("tasks")
+                .whereEqualTo("tID", user_id.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            // Log.e(TAG, "fetchTodos failed", task.getException());
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot todo : task.getResult()) {
+                            String name = todo.get("task").toString();
+                            insertTodoUi(name);
+                        }
+                    }
+                });
+    }
+
+    private void insertTodoUi(String name) {
+        // Add to View
+       // TextView todo = new TextView(getApplicationContext());
+
+        tasks = tasks + "\n" + name ;
+        todo.setText(tasks);
+
+
+
+    }
+
+
 }
